4ed766a13f8a1b47341878a000513fa843856e8f
Adill Al-Ashgar <adillwmaa@gmail.com>
2022-10-10 03:07:01 +0100

    "First Test Commit"

---
+++ DeepClean_Main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+LHC-B Torch ML Noise Reduction v0.0.1
+Created on Mon Oct 10 2022
+@authors: Adill Al-Ashgar & Max Carter
+
+"""
+
+#%% - Dependencies
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
