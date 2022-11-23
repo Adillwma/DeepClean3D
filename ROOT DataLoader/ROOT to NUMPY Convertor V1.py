2edf0c67737df4cd4e3687fd4bedae1e76ef6a99
Adill Al-Ashgar <adillwmaa@gmail.com>
2022-11-23 16:24:38 +0000

    Update ROOT to NUMPY Convertor V1.py

--- ROOT DataLoader/ROOT to NUMPY Convertor V1.py
+++ ROOT DataLoader/ROOT to NUMPY Convertor V1.py
@@ -8,4 +8,10 @@ Created on Tue Nov 23 2022
 #%% - Dependencies
 import matplotlib.pyplot as plt
 import numpy as np
+import uproot
 
+
+with uproot.open("path/to/dataset.root") as file:
+    do_something...
+
+    
