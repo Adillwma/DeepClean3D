d5c44b5a1fbde8f87b8b577240919cfd8b5defb3
Adill Al-Ashgar <adillwmaa@gmail.com>
2022-11-23 16:34:02 +0000

    Update ROOT to NUMPY Convertor V1.py

--- ROOT DataLoader/ROOT to NUMPY Convertor V1.py
+++ ROOT DataLoader/ROOT to NUMPY Convertor V1.py
@@ -10,8 +10,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import uproot
 
+###Optimisation information: 
+# The uproot.open function has many options, including alternate handlers for each input type, num_workers to control 
+# parallel reading, and caches (object_cache and array_cache). The defaults attempt to optimize parallel processing, 
+# caching, and batching of remote requests, but better performance can often be obtained by tuning these parameters.
 
-with uproot.open("path/to/dataset.root") as file:
-    do_something...
 
-    
+
+path = #Path for root file
+with uproot.open(path) as file:
+    for data_item in file:
+        #np_data_item = ???? #Turn data_item into numpy array if it is not already    
+        
+        savepath = path + '/processed'
+        np.save(np_data_item, savepath)
+
