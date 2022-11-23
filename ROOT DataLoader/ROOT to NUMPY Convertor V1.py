d6b77683e69edb5de0d3b63855a13f4d84c6c25b
Adill Al-Ashgar <adillwmaa@gmail.com>
2022-11-23 16:17:41 +0000

    Root to Numpy Calc v0.0.1

---
+++ ROOT DataLoader/ROOT to NUMPY Convertor V1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+ROOT to NUMPY Convertor
+@author: Max Carter
+Created on Tue Nov 23 2022
+"""
+
+#%% - Dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+
