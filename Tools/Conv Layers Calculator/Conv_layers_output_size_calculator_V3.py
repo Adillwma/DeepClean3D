4e470cbba2b3079fbcf124907ace457cba201628
Adill Al-Ashgar <adillwmaa@gmail.com>
2022-12-12 17:28:41 +0000

    Updated Function

--- Tools/Conv Layers Calculator/Conv_layers_output_size_calculator_V3.py
+++ Tools/Conv Layers Calculator/Conv_layers_output_size_calculator_V3.py
@@ -5,7 +5,7 @@ Convolutional Layer Output Size Calculator V3.0
 Created on Fri Nov 11 02:45:06 2022
 
 USER NOTICE!
-x To use this code standalone as a calculator, just set the parameters below in 'User Inputs' section, 
+x //CHANGE THIS ADVICE// To use this code standalone as a calculator, just set the parameters below in 'User Inputs' section, 
   then run the entire code, it should then print your results to terminal.
 
 x You can also import this function into another page by putting it into the same 
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 from math import floor
 
 #%% - User Inputs
+"""
 conv_type = 1 #Select conv type: 0=conv2D, 1=conv2D.Transpose, 2=conv3D, 3=conv3D.Transpose (WARNING: Values other than 0-3 will select conv3D.Transpose)
 
 H_in = 7         # height of the inputs
@@ -31,7 +32,7 @@ O = (0,1)           # Output padding (used only in the conv Transpose )
 #Note: Currently channels are unused in this script, possible improvment? Can add layer channel/parameter calculation from it?
 C_in = 16           # number of input channels
 C_out = 32          # number of output channels
-
+"""
 
 
 #%% - Helper Functions
@@ -128,9 +129,10 @@ def conv_calculator(conv_type, K, P, S, D, H_in, W_in, D_in=0, O=1):
     return(outputs)
 
 #%% - Outputs
-#conv_calculator(conv_type, K, P, S, D, H_in, W_in, D_in, O)
+
 start_size = 128, 88
 Kernal = (3,3)
+
 CL1 = conv_calculator(conv_type=0, K=Kernal, P=0, S=2, D=1, H_in=start_size[0], W_in=start_size[1], D_in=0, O=0)
 CL2 = conv_calculator(conv_type=0, K=Kernal, P=0, S=2, D=1, H_in=CL1[0], W_in=CL1[1], D_in=0, O=0)
 CL3 = conv_calculator(conv_type=0, K=Kernal, P=(0,1), S=2, D=1, H_in=CL2[0], W_in=CL2[1], D_in=0, O=0)
@@ -152,66 +154,3 @@ print ("Conv.T 1 Size:", CLT1)
 print ("Conv.T 2 Size:", CLT2)
 print ("Conv.T 3 Size:", CLT3)
 print ("Conv.T 4 Size:", CLT4)
-#%% - Automated Testing    
-run_debugging_tests = 0 #Set to 1 to run the tests, 0 to turn off
-
-if run_debugging_tests == 1:
-    #####TESTS#####
-    input_size_range = (1,100) #Range to test accross
-    kernal_size_range = (1,100) #Range to test accross
-    stride_range = (1,100) #Range to test accross
-    padding_range = (0,100) #Range to test accross
-    
-    input_size = 5 #Default value to use when held as constant
-    kernal_size = 3 #Default value to use when held as constant
-    stride = 1 #Default value to use when held as constant
-    padding = 1 #Default value to use when held as constant
-    dilation = 1 #Default value to use when held as constant
-    square_conv_outputs(input_size, padding, dilation, kernal_size, stride)
-    
-    insize_data = []
-    kersize_data = []
-    stride_data = []
-    pad_data = []
-    
-    idx_data = []
-    kdx_data = []
-    sdx_data = []
-    pdx_data = []
-    
-    irange = range(input_size_range[0], input_size_range[1]+1)
-    krange = range(kernal_size_range[0], kernal_size_range[1]+1)
-    srange = range(stride_range[0], stride_range[1]+1)
-    prange = range(padding_range[0], padding_range[1]+1)
-    
-    for idx, input_size in enumerate(irange):
-        insize_data.append(square_conv_outputs(input_size, padding, dilation, kernal_size, stride))
-        idx_data.append(idx)
-        
-    plt.plot(idx_data, insize_data)
-    plt.title("input_size")
-    plt.show()
-    
-    for kdx, kernal_size in enumerate(krange):
-        kersize_data.append(square_conv_outputs(input_size, padding, dilation, kernal_size, stride))
-        kdx_data.append(kdx)
-        
-    plt.plot(kdx_data, kersize_data)
-    plt.title("kernal_size")
-    plt.show()
-            
-    for sdx, stride in enumerate(srange):
-        stride_data.append(square_conv_outputs(input_size, padding, dilation, kernal_size, stride))
-        sdx_data.append(sdx)
-        
-    plt.plot(sdx_data, stride_data)
-    plt.title("stride")
-    plt.show()
-        
-    for pdx, padding in enumerate(prange):
-        pad_data.append(square_conv_outputs(input_size, padding, dilation, kernal_size, stride))
-        pdx_data.append(pdx) 
-        
-    plt.plot(pdx_data, pad_data)
-    plt.title("padding")
-    plt.show()
