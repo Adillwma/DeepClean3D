ac6e9dc7fbe6ba271372a1a611b7bbe36e89222a
CarterM143 <maxscarter99@gmail.com>
2023-01-27 15:47:14 +0000

    Renaming
    Renamed things as were errors calling them

--- Data Generators/Realistic Data Generator/Realistic Dataset Generator V1.py
+++ Data Generators/Realistic Data Generator/Realistic Dataset Generator V1.py
@@ -50,7 +50,7 @@ coord_transform_sig_fig = 12    #Setting significant figures for the coordinate 
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-from Realistic_Data_Simulator_V1 import realistic_data_generator
+from Realistic_Data_Simulator_V11 import realistic_data_generator
 
 #%% - Helper Functions
 def pol2cart(magnitude, angle, significant_figures = 12):                     
