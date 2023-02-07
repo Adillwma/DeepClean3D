2a4472d2b6f5e6d1895476fd7b4f16ed4fae296c
CarterM143 <maxscarter99@gmail.com>
2023-02-07 12:47:31 +0000

    Simple Generator
    Simple generator made and route to save documents created

--- Data Generators/Simple Cross/supsimp_x_sim.py
+++ Data Generators/Simple Cross/supsimp_x_sim.py
@@ -3,8 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def simp_generator(sig_pts = 28, n_pts = 50, x_dim = 28, y_dim = 28, z_dim = 28, setting = 'Dirty'):
-        
+def simp_simulator(sig_pts = 28, n_pts = 0, x_dim = 28, y_dim = 28, z_dim = 28):
+    """
+    This generator function generates crosses across the dimensions of the volume. (seeds to be generalised for non-perfect 28x28).
+    It returns an array consisting of two joined arrays:
+    1st is the cross with no noise, to calculate loss etc.
+    2nd is the cross with noise, thats to be cleaned.
+    (The noise is random ever time its called)
+
+    sig_pts - number of signal points
+    n_pts - number of noise points
+    x_dim - x axis dimentions (pixels)
+    y_dim - y axis dimentions (pixels)
+    z_dim - z axis dimentions (pixels)
+
+    """
     # define min and max of graph (pixels between 0 and 27)
     x_min = 0
     x_max = x_dim - 1     
@@ -44,8 +57,8 @@ def simp_generator(sig_pts = 28, n_pts = 50, x_dim = 28, y_dim = 28, z_dim = 28,
     z1_array = np.linspace(z1_data_points[0], z1_data_points[1], int(sig_pts/2))
 
     L1_comb = np.column_stack((x1_array, y1_array, z1_array))      # joins them all together. Should be 28 at each point 0 to 28:
-    print(np.shape(L1_comb))
-    print(L1_comb[0])
+    # print(np.shape(L1_comb))
+    # print(L1_comb[0])
 
     # for line2:
     x2_array = np.linspace(x2_data_points[0], x2_data_points[1], int(sig_pts/2))
@@ -53,54 +66,56 @@ def simp_generator(sig_pts = 28, n_pts = 50, x_dim = 28, y_dim = 28, z_dim = 28,
     z2_array = np.linspace(z2_data_points[0], z2_data_points[1], int(sig_pts/2))
 
     L2_comb = np.column_stack((x2_array, y2_array, z2_array))      # joins them all together. Should be 28 at each point 0 to 28:
-    print(np.shape(L2_comb))
-    print(L2_comb[1])
+    # print(np.shape(L2_comb))
+    # print(L2_comb[1])
 
     # make final combined np array
     hits_comb = np.concatenate((L1_comb, L2_comb))
-    print(np.shape(hits_comb))
 
     #-------------------------------------------------------------------
 
     # flattening the data
 
     # this creates a 28x28 zeros array  (plus 1 as max is 27.)
-    flattened_data = np.zeros((x_max + 1, y_max + 1))
+    flattened_data = np.zeros((2,x_dim, y_dim))
 
-    # noise:
-    all_pts = hits_comb
-    for _ in range(n_pts):
-        all_pts.append(np.random.randint(0,x_max), np.random.randint(0,y_max), np.random.randint(0,z_max))
+    # random noise in x, y, z
+    # random.randint(low, high=None, size=None, dtype=int)
+    noise_pts_x = np.random.randint(0,x_max, n_pts)
+    noise_pts_y = np.random.randint(0,y_max, n_pts)
+    noise_pts_z = np.random.randint(0,z_max, n_pts)
+
+    # join to one list like with hits:
+    noise = np.column_stack((noise_pts_x, noise_pts_y, noise_pts_z))
+    # and join hits and noise to make list of signal noise (SN) points :
+    SN_pts = np.concatenate((hits_comb, noise))
     
     ###############################################################################
-    # if setting is Dirty, adds noise, if Clean, no noise.
-    if setting == 'Clean':
-        for point in hits_comb:
-            # TOF is the z axis
-            TOF = point[2]
-            # index is the x and y axis
-            flattened_data[int(point[0])][int(point[1])] = TOF
-    elif setting == 'Dirty':
-        for point in all_pts:
-            # TOF is the z axis
-            TOF = point[2]
-            # index is the x and y axis
-            flattened_data[int(point[0])][int(point[1])] = TOF
+    # if setting is Noisy, adds noise, if Clean, no noise.
 
-    # plot 2d data
-    plt.imshow(flattened_data)
-    plt.show()
+    for point in hits_comb:
+        # TOF is the z axis
+        TOF = point[2]
+        # index is the x and y axis
+        flattened_data[0][int(point[0])][int(point[1])] = TOF
+    
+    for point in SN_pts:
+        # TOF is the z axis
+        TOF = point[2]
+        # index is the x and y axis
+        flattened_data[1][int(point[0])][int(point[1])] = TOF
+    
+    return flattened_data
 
-#------------------------------------------------------------------
-simp_generator(sig_pts = 28, n_pts=50, x_dim = 28, y_dim = 28, z_dim = 28, setting = 'Dirty')
-
-# plot cross
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# # line plotted between min and max of both lines
-# ax.plot([x[0] for x in L1_comb], [y[1] for y in L1_comb], [z[2] for z in L1_comb])
-# ax.plot([x[0] for x in L2_comb], [y[1] for y in L2_comb], [z[2] for z in L2_comb])
-# plt.show()
 
 #------------------------------------------------------------------
+# calling function and plotting results for clean and noisy:
+
+flattened_data = simp_simulator(sig_pts = 28, n_pts=50, x_dim = 28, y_dim = 28, z_dim = 28)
+
+# plot 2d clean data
+plt.imshow(flattened_data[0])
+plt.show()
+# plot 2d noisy data
+plt.imshow(flattened_data[1])
+plt.show()
