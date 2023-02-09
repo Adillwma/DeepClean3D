eeaff3b2e0ed233ffc1820d7eb0769c4e99bd428
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-02-09 19:41:49 +0000

    Updated folder structure, duplicated 28x28 net, cleaned up old files

---
+++ 2D & 3D DataLoader/Testing.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+import random
+
+
+# print(torch.tensor(lst))
+
+# a = 1.3
+# print(type(a))
+
+# print(random.randint(1,2))
+
+# a = np.array(((1,2,3), (1,2,3)))
+
+# file = "TEST_REALISTIC_Data/"
+# dir = "C:/Users/maxsc/OneDrive - University of Bristol/3rd Year Project/Autoencoder/2D 3D simple version/Circular and Spherical Dummy Datasets/"
+# np.save(dir + file + 'a', a)
+# x = np.load(dir+file+'a.npy')
+# # print(x)
+# a = [1,2,3,4,-6]
+
+# print(min(a))
+
+print(np.linspace(0,7,4))
