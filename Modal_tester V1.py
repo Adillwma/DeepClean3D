e00618d741bbdde8e59ff6cd8999ec34b2e65704
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-02-20 23:06:21 +0000

    Created Model integrity test

---
+++ Modal_tester V1.py
@@ -0,0 +1,17 @@
+"""
+### Modal_tester V1
+# If the saved model is successfully loaded, the program will print out the contents of the model. 
+# If the "AttributeError" occurs, you can try to investigate the saved model file to check whether it 
+# contains the expected "Encoder" attribute. You can also check the code used to save the model to ensure 
+# that the "Encoder" attribute is included.
+"""
+import torch
+
+model = torch.load("Models/10X_Activation_V1.pth")
+
+print(model)
+
+
+
+
+
