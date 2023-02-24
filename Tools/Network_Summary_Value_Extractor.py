d601143b6ccf645e0b062379bb36b43ec99f00a4
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-02-24 17:32:02 +0000

    Updated to also grab Train and Val loss

--- Tools/Network_Summary_Value_Extractor.py
+++ Tools/Network_Summary_Value_Extractor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 22 2022
-Time_Scraper V1.0.0
+Net Summary Value Extractors V1.0.0
 Author: Adill Al-Ashgar
 University of Bristol
 
@@ -10,15 +10,39 @@ University of Bristol
 
 # Scrape Training Time
 def get_training_time(file_path):
+    "Time_Scraper V1.0.0"
     with open(file_path, 'r') as f:                                   # Opens the file at the given file path using a with to ensure proper file handline (makes sure file is closed properly)
         for line in f:                                                # Iterates through file line by line
             if 'Training Time:' in line:                              # For each line, checks if the string "Training Time:" is present
-                time_str = line.strip().split(': ')[1].split(' ')[0]  # If it is, it extracts the training time string, which is the value after the colon and before the first space character
-                return float(time_str)                                # Converts the extracted string to a float and returns it
+                Dstr = line.strip().split(': ')[1].split(' ')[0]  # If it is, it extracts the training time string, which is the value after the colon and before the first space character
+                return float(Dstr)                                # Converts the extracted string to a float and returns it
     print('Training Time not found in file')               # If the function reaches the end of the file without finding the training time string, it warns user but does not break for error, the function will return a flag so main body program can implement its own conditions if neded.
     return ("Error")
-"""
+
 # Test Driver
 training_time = get_training_time('path/to/file.txt')
 print(training_time)
-"""
+
+
+# Val Loss
+def get_valloss_time(file_path):
+    "Val_Loss_Scraper V1.0.0"
+    with open(file_path, 'r') as f:                                   # Opens the file at the given file path using a with to ensure proper file handline (makes sure file is closed properly)
+        for line in f:                                                # Iterates through file line by line
+            if 'Val Loss:' in line:                              # For each line, checks if the string "Training Time:" is present
+                Dstr = line.strip().split(': ')[1].split(' ')[0]  # If it is, it extracts the training time string, which is the value after the colon and before the first space character
+                return float(Dstr)                                # Converts the extracted string to a float and returns it
+    print('Val Loss not found in file')               # If the function reaches the end of the file without finding the training time string, it warns user but does not break for error, the function will return a flag so main body program can implement its own conditions if neded.
+    return ("Error")
+
+
+# Train Loss
+def get_trainloss_time(file_path):
+    "Train_Loss_Scraper V1.0.0"
+    with open(file_path, 'r') as f:                                   # Opens the file at the given file path using a with to ensure proper file handline (makes sure file is closed properly)
+        for line in f:                                                # Iterates through file line by line
+            if 'Train Loss:' in line:                              # For each line, checks if the string "Training Time:" is present
+                Dstr = line.strip().split(': ')[1].split(' ')[0]  # If it is, it extracts the training time string, which is the value after the colon and before the first space character
+                return float(Dstr)                                # Converts the extracted string to a float and returns it
+    print('Train Loss not found in file')               # If the function reaches the end of the file without finding the training time string, it warns user but does not break for error, the function will return a flag so main body program can implement its own conditions if neded.
+    return ("Error")
