0290c838b26e428a7ac0f058be1798681464bdc7
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-02-24 21:09:22 +0000

    Updated visualizations, separated functions for each so that user can control which plots exactly they desire, also updated to include the plot or save functionality so plots can be autosaved without user intervention

--- DC3D_V3/AE_Visulisations.py
+++ DC3D_V3/AE_Visulisations.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-AE_Visulisations V1.0.0
+AE_Visulisations V2.0.0
 Created: 20:56 13 Feb 2023
 Author: Adill Al-Ashgar
 University of Bristol
@@ -26,23 +26,36 @@ from sklearn.manifold import TSNE
 
 #%% - Differnce between images
 ####Would be cool to run this similarity function every plotting cycle in the training and then plot over time the number of mathching elemets to see if we ever get any perfect pixel reconstructions
-
-def AE_visual_difference(image, noised_image, cleaned_image, plot=False, print_text=False):
-    def plot_image_diff(img1, img2, plot):
-        #Calculates and plots the pixel-wise difference between two images of the same size.
+def AE_visual_difference(image, noised_image, cleaned_image, print_text=False):
+    """
+    Calculates and plots the pixel-wise difference between three images and returns statistics.
+    
+    Parameters:
+    image (ndarray): An image.
+    noised_image (ndarray): Same image with noise added.
+    cleaned_image (ndarray): The same image now denoised.
+    print_text (bool): Whether to print text to console summarizing the differences between the images.
+    
+    Returns:
+    num_diff_noised (Int): Number of different elements between `image` and `noised_image`
+    num_same_noised (Int): Number of same elements between `image` and `noised_image`
+    num_diff_cleaned (Int): the number of different elements between `image` and `cleaned_image`
+    num_same_cleaned (Int): the number of same elements between `image` and `cleaned_image`
+    im_diff_noised (ndarray): Pixel-wise difference between `image` and `noised_image`.
+    im_diff_cleaned (ndarray): Pixel-wise difference between `image` and `cleaned_image`.
+    """
+    
+    def image_diff(img1, img2):
+        """
+        Calculates and plots the pixel-wise difference between two images of the same size.
+        """
+        # Check image sizes are the same
         if img1.shape != img2.shape:
             raise ValueError("The two images must have the same shape.")
         
         # Calculate the pixel-wise difference between the two images
         diff = np.abs(img1.numpy().squeeze().squeeze() - img2.numpy().squeeze().squeeze())  #takes from input of [batchsize, channels, x, y] to [x, y]
-        
-        if plot == True:
-            # Plot the difference image as an imshow plot
-            plt.imshow(diff, cmap='gray')
-            plt.title("Pixel-wise difference between the two images")
-            plt.axis('off')
-            plt.show()
-        
+
         # Count the total number of elements
         num_total = diff.size
 
@@ -52,21 +65,20 @@ def AE_visual_difference(image, noised_image, cleaned_image, plot=False, print_t
         # Calculate number of similar elements 
         num_same = num_total - num_diff
 
-        return num_diff, num_same
+        return diff, num_diff, num_same
 
     # Calculate and plot the pixel-wise difference between the two images
-    num_diff_noised, num_same_noised = plot_image_diff(image, noised_image, plot)
-    num_diff_cleaned, num_same_cleaned = plot_image_diff(image, cleaned_image, plot)
+    im_diff_noised, num_diff_noised, num_same_noised = image_diff(image, noised_image)
+    im_diff_cleaned, num_diff_cleaned, num_same_cleaned = image_diff(image, cleaned_image)
 
     if print_text:
         print("\nNumber of different elements between original and noised:", num_diff_noised, "\nNumber of same elements between original and noised", num_same_noised, "\n")
         print("Number of different elements between original and cleaned:", num_diff_cleaned, "\nNumber of same elements between original and cleaned", num_same_cleaned, "\n")
 
-    return (num_diff_noised, num_same_noised, num_diff_cleaned, num_same_cleaned)
+    return (num_diff_noised, num_same_noised, num_diff_cleaned, num_same_cleaned, im_diff_noised, im_diff_cleaned)
 
-
-def AE_visulisation(encoder, decoder, latent_dim, device, test_loader, test_dataset, batchsize):
-    #%% - GraphViz
+#%% - GraphViz
+def Graphwiz_visulisation():
     """
     Using GraphViz: GraphViz is a popular open-source graph visualization software that can be used to visualize the 
     structure of your PyTorch autoencoder network. You can use the torchviz package to generate a GraphViz dot file from 
@@ -98,11 +110,22 @@ def AE_visulisation(encoder, decoder, latent_dim, device, test_loader, test_data
 
     """
 
+#%% - Create new images from the latent space
+def Generative_Latent_information_Visulisation(encoder, decoder, latent_dim, device, test_loader):
+    """
+    Function to visualize autoencoder data by generating random latent vectors and reconstructing 
+    corresponding images using the decoder model.
 
-    #%% - Create new images from the latent space
-    def show_image(img):
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    Parameters:
+        encoder (torch.nn.Module): The encoder model.
+        decoder (torch.nn.Module): The decoder model.
+        latent_dim (int): The dimensionality of the latent space.
+        device (str): The device to run the computations on.
+        test_loader (torch.utils.data.DataLoader): The data loader containing the test dataset.
+
+    Returns:
+
+    """
 
     encoder.eval()
     decoder.eval()
@@ -126,12 +149,24 @@ def AE_visulisation(encoder, decoder, latent_dim, device, test_loader, test_data
         latent = latent.to(device)
         img_recon = decoder(latent)
         img_recon = img_recon.cpu()
-
-        fig, ax = plt.subplots(figsize=(20, 8.5))
-        show_image(torchvision.utils.make_grid(img_recon[:100],10,10, pad_value=100))
-        plt.show()
         
-    #%% - Plots for the higher dimensional space
+        return (img_recon)
+
+#%% - Plots for the higher dimensional space
+def Reduced_Dimension_Data_Representations(encoder, device, test_dataset, plot_or_save=0):
+    """
+    Display the input data samples as a XXXXX
+
+
+    Parameters:
+        encoder (torch.nn.Module): The encoder model.
+        device (str): The device to run the computations on.
+        test_dataset (TYPE???): The test dataset.
+        plot_or_save (int, optional): Specifies whether to display the visualization (0) or save it to file (1) or both (2).
+
+    Returns:
+
+    """
     encoded_samples = []
     for sample in tqdm(test_dataset):
         img = sample[0].unsqueeze(0).to(device)
@@ -146,20 +181,16 @@ def AE_visulisation(encoder, decoder, latent_dim, device, test_loader, test_data
             encoded_sample['label'] = label
             encoded_samples.append(encoded_sample)
     encoded_samples = pd.DataFrame(encoded_samples)
-    encoded_samples
 
     ### Higher dim
-    fig = px.scatter(encoded_samples, x='Enc. Variable 0', y='Enc. Variable 1',
-            color=encoded_samples.label.astype(str), opacity=0.7)
-    fig.show()
+    encoded_samples
 
     ### TSNE of Higher dim
     tsne = TSNE(n_components=2)
     tsne_results = tsne.fit_transform(encoded_samples.drop(['label'],axis=1))
-    fig2 = px.scatter(tsne_results, x=0, y=1,
-                    color=encoded_samples.label.astype(str),
-                    labels={'0': 'tsne-2d-one', '1': 'tsne-2d-two'})
-    fig2.show()
+
+    return(encoded_samples, tsne_results)
+
 
 
 
