0dcbd76f687f15cd35d5871022c080e0559ab9dc
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-03-02 18:25:06 +0000

    Update Make_Dataset_Fast.py

--- Data Generators/Make_Dataset_Fast.py
+++ Data Generators/Make_Dataset_Fast.py
@@ -8,15 +8,14 @@ time_dimension = 100
 sig_pts = 28
 output_dir = ###
 
-number_of_blanks = 10
-number_single_x = 
-number_of_two_x = 
-number_of_three_x = 
-number_of_four_x = 
+number_of_blanks = 2
+number_single_x = 2
+number_of_two_x = 2
+number_of_three_x = 2
+number_of_four_x = 2
 
 shift_x_positions = True
 rotate_x_positions = True
-
 
 
 #%% - Compute
@@ -26,14 +25,17 @@ generate_blanks(xdim=xdim, ydim=ydim, number_of_files=number_of_blanks, output_d
 # Gen Signals  - can clean up all the intro code to one line if sort out args slightly (needs number of images removed and then no ratios to types of images just straiht values, and finally the shift and rotate params dshould accept boolean instead of 0-1 for user ease of following code)
 total_x_images = number_single_x + number_of_two_x + number_of_three_x + number_of_four_x
 datasplit_ratios = [number_single_x/total_x_images, number_of_two_x/total_x_images, number_of_three_x/total_x_images, number_of_four_x/total_x_images]
+
 if shift_x_positions:
     shift = 1
 else:
     shift = 0
+
 if rotate_x_positions:
     rotate = 1
 else:
     rotate = 0
+
 simp_generator(datasplit_ratios, total_x_images, sig_pts, xdim, ydim, time_dimension, shift, rotate)
 
 
