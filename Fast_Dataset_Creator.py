89864d1b35af03be393fbe7443e8eee29dd14997
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-04-03 15:38:46 +0100

    Major update to include multi sig realistic signals generation, bumped version to V2

--- Fast_Dataset_Creator.py
+++ Fast_Dataset_Creator.py
@@ -10,12 +10,12 @@ University of Bristol
 """
 
 #%% - User Inputs
-xdim = 88                                           # X dimension of image
-ydim = 128                                          # Y dimension of image
-time_dimension = 100                                # Time dimension of image
-sig_pts = 50                                        # Number of signal points to generate
-data_set_title = "Dataset 32_X10Ks Sparse SIG50"    # Ouput title of dataset
-data_sets_folder =r"N:\Yr 3 Project Datasets\\"     # Folder to save dataset to
+xdim = 88                                            # X dimension of image
+ydim = 128                                           # Y dimension of image
+time_dimension = 100                                 # Time dimension of image
+sig_pts = 300                                         # Number of signal points to generate
+data_set_title = "Dataset 34_Real10Ks RANDOM"     # Ouput title of dataset
+data_sets_folder = r"N:\Yr 3 Project Datasets\\"     # Folder to save dataset to
 
 #Ada: r"C:\Users\Student\Documents\UNI\Onedrive - University of Bristol\Yr 3 Project\Circular and Spherical Dummy Datasets\\"
 #Max: 
@@ -24,12 +24,12 @@ data_sets_folder =r"N:\Yr 3 Project Datasets\\"     # Folder to save dataset to
 number_of_blanks = 0            # Number of blank data files to 
 number_of_hemisphere = 0        # Number of hemisphere data files to generate
 
-number_single_x = 10000         # Number of single x data files to generate
+number_single_x = 0         # Number of single x data files to generate
 number_of_two_x = 0             # Number of two x data files to generate
 number_of_three_x = 0           # Number of three x data files to generate
 number_of_four_x = 0            # Number of four x data files to generate
 
-number_single_realisitic = 0    # Number of realistic data files to generate
+number_single_realisitic = 10000    # Number of realistic data files to generate
 number_of_two_realisitic = 0    # Number of two realistic data files to generate
 number_of_three_realisitic = 0  # Number of three realistic data files to generate
 number_of_four_realisitic = 0   # Number of four realistic data files to generate
@@ -42,48 +42,49 @@ rotate_x_positions = False     #If True then the x signals are rotated, otherwis
 rotate_seperatly = True        #If True then each x signal is rotated by a random amount, otherwise they are all rotated by the same amount (only used if rotate_x_positions is True)
 
 #Realistic Gen Settings
-ideal_hit_spread = True        #If True then the hit points spread is idealised as linearly spaced in the pattern, otherwise points are chosen randomly from pattern 
+ideal_hit_spread = False        #If True then the hit points spread is idealised as linearly spaced in the pattern, otherwise points are chosen randomly from pattern 
 
 #%% - Dependencies
 #External Libraries
 import datetime          # Current local time and date
 
 # Local Custom Generators
-from Data_Generators.Comb_X_Gen_Full import simp_generator
-from Data_Generators.Blank_Data_Image_Generator import generate_blanks
-from Data_Generators.Realistic_Gen import multi_real_gen_wrapper
-#from Data_Generators.Hemispheres_Gen import 
+from Data_Generators.Comb_X_Gen_Full import simp_generator                     # Import custom function for generating X signals
+from Data_Generators.Blank_Data_Image_Generator import generate_blanks         # Import custom function for generating blank images
+from Data_Generators.Realistic_Gen import multi_real_gen_wrapper               # Import custom function for generating realistic TORCH signals
+#from Data_Generators.Hemispheres_Gen import hemispheres_gen_wrapper           # Import custom function for generating hemisphere signals
+#from Data_Generators.spherical                                                # Import custom function for generating spherical signals
 
 # Local Dataset Validation Functions
-from DC3D_V3.Helper_files.Dataset_distribution_tester_V1 import dataset_distribution_tester
-from DC3D_V3.Helper_files.Dataset_Integrity_Check_V1 import dataset_integrity_check
+from DC3D_V3.Helper_files.Dataset_distribution_tester_V1 import dataset_distribution_tester  # Import custom function for testing the distribution of the dataset
+from DC3D_V3.Helper_files.Dataset_Integrity_Check_V1 import dataset_integrity_check          # Import custom function for testing the integrity of values in the dataset
 
 #%% - Program Initalisation
 # Combine output folder and dataset information folder
-output_dir = data_sets_folder + data_set_title + "\Data\\"
-output_dataset_info = data_sets_folder + data_set_title + " Information.txt" 
-output_dataset_dist_img = data_sets_folder + data_set_title
+output_dir = data_sets_folder + data_set_title + "\Data\\"     # Add the data folder to the end of the output directory
+output_dataset_info = data_sets_folder + data_set_title + " Information.txt"   # Add the information file to the end of the output directory
+output_dataset_dist_img = data_sets_folder + data_set_title    
 
 #%% - Generators
 # Generate Blanks
-generate_blanks(xdim=xdim, ydim=ydim, number_of_files=number_of_blanks, output_dir=output_dir)
+generate_blanks(xdim=xdim, ydim=ydim, number_of_files=number_of_blanks, output_dir=output_dir)   # Generate blank images
 
 # Generate X signals
-datasplit_values = [number_single_x, number_of_two_x, number_of_three_x, number_of_four_x]
-simp_generator(output_dir, datasplit_values, sig_pts, ydim, xdim, time_dimension, shift_positions, rotate_x_positions)    #needs a output dir arg
+datasplit_values = [number_single_x, number_of_two_x, number_of_three_x, number_of_four_x]   # List of number of each type of X signal to generate
+simp_generator(output_dir, datasplit_values, sig_pts, ydim, xdim, time_dimension, shift_positions, rotate_x_positions)    # Generate X signals
 
 #Generate Realisitic Signals
-realistic_proportions = [number_single_realisitic, number_of_two_realisitic, number_of_three_realisitic, number_of_four_realisitic]
-multi_real_gen_wrapper(output_dir, realistic_proportions, signal_points=sig_pts, detector_pixel_dimensions=(ydim, xdim), hit_point='random', ideal=ideal_hit_spread, debug_image_generator=False, shift=shift_positions)
+realistic_proportions = [number_single_realisitic, number_of_two_realisitic, number_of_three_realisitic, number_of_four_realisitic]  # List of number of each type of realistic signal to generate
+multi_real_gen_wrapper(output_dir, realistic_proportions, signal_points=sig_pts, detector_pixel_dimensions=(ydim, xdim), hit_point='random', ideal=ideal_hit_spread, debug_image_generator=False, shift=shift_positions)  # Generate realistic signals
 
-print("Dataset Generation Completed")
+print("Dataset Generation Completed")   # Print to console to show that dataset generation has completed
 #%% - Run Tests
 try:
     print("\nVerifying Dataset Integrity...")
-    integrity_results = dataset_integrity_check(output_dir, full_test=True, print_output=True)
+    integrity_results = dataset_integrity_check(output_dir, full_test=True, print_output=True)  # Run dataset integrity test
     
     print("\nVerifying Dataset TOF Distribution...")
-    distribution_results = dataset_distribution_tester(output_dir, time_dimension=time_dimension, ignore_zero_vals_on_plot=True, output_image_dir=output_dataset_dist_img)
+    distribution_results = dataset_distribution_tester(output_dir, time_dimension=time_dimension, ignore_zero_vals_on_plot=True, output_image_dir=output_dataset_dist_img) # Run dataset distribution test
 
 except:                             #If test itself fails then result is automatically set to fail with unknown cause.
     dataset_tests_fail = True
@@ -100,7 +101,7 @@ with open(output_dataset_info, 'w', encoding='utf-8') as output_file:    #utf_8 
     output_file.write(f"Date dataset created: {TD_now.strftime('%Y-%m-%d %H:%M:%S')}\n")    
     output_file.write((f"Dataset ID: {data_set_title}\n"))
     
-    output_file.write("\nInput Settings:\n")
+    output_file.write("\nInput Settings:\n")            # Write the input settings to the file
     output_file.write((f"X dim: {xdim} pixels\n"))
     output_file.write((f"Y dim: {ydim} pixels\n"))
     output_file.write((f"Time dim: {time_dimension}\n"))
@@ -110,14 +111,18 @@ with open(output_dataset_info, 'w', encoding='utf-8') as output_file:    #utf_8 
     output_file.write((f"Rotate Seperatly: {rotate_seperatly}\n"))
     output_file.write((f"Realistic Ideal Hit Spread: {ideal_hit_spread}\n"))
 
-    output_file.write("\nDataset Blend:\n")
+    output_file.write("\nDataset Blend:\n")                 # Write the dataset blend to the file
     output_file.write((f"Blank Files: {number_of_blanks}\n"))
-    output_file.write((f"One Cross: {number_single_x}\n"))
+    output_file.write((f"Hemisphere: {number_of_hemisphere}\n"))
+    output_file.write((f"\nOne Cross: {number_single_x}\n"))
     output_file.write((f"Two Crosses: {number_of_two_x}\n"))
     output_file.write((f"Three Crosses: {number_of_three_x}\n"))
     output_file.write((f"Four Crosses: {number_of_four_x}\n"))
-    output_file.write((f"Hemisphere: {number_of_hemisphere}\n"))
-    output_file.write((f"Realistic: {number_single_realisitic}\n"))
+    
+    output_file.write((f"\nOne Realistic Signal: {number_single_realisitic}\n"))
+    output_file.write((f"Two Realistic Signals: {number_of_two_realisitic}\n"))
+    output_file.write((f"Three Realistic Signals: {number_of_three_realisitic}\n"))
+    output_file.write((f"Four Realistic Signals: {number_of_four_realisitic}\n"))
 
     output_file.write("\nDataset Test Results:\n")
     for key, value in integrity_results.items():
