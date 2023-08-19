202cac138ac44681a8371baf4c56da67d8c7e7d6
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-08-19 18:09:23 +0100

    Added user control for linear network input and output size

--- DC3D_V3/Autoencoders/DC3D_Autoencoder_V1_Protected2_2.py
+++ DC3D_V3/Autoencoders/DC3D_Autoencoder_V1_Protected2_2.py
@@ -21,11 +21,13 @@ from torch import nn
 #%% - Encoder
 class Encoder(nn.Module):
     
-    def __init__(self, encoded_space_dim, encoder_debug):
+    def __init__(self, encoded_space_dim, encoder_debug, fc_layer_size=128):
         super().__init__()
 
         # Enables encoder layer shape debug printouts
         self.encoder_debug = encoder_debug
+
+        self.fc_layer_size = fc_layer_size
 
         self.encoder_cnn = nn.Sequential(
             # N.B. input channel dimensions are not the same as output channel dimensions:
@@ -48,11 +50,11 @@ class Encoder(nn.Module):
         ###Linear Encoder Layers
         self.encoder_lin = nn.Sequential(
             #Linear encoder layer 1  
-            nn.Linear(32 * 16 * 11, 128),                   #!!! linear network layer. arguuments are input dimensions/size, output dimensions/size. Takes in data of dimensions 3* 3 *32 and outputs it in 1 dimension of size 128
+            nn.Linear(32 * 16 * 11, self.fc_layer_size),                   #!!! linear network layer. arguuments are input dimensions/size, output dimensions/size. Takes in data of dimensions 3* 3 *32 and outputs it in 1 dimension of size 128
             # nn.Linear(L3[0] * L3[1] * 32, 128),
             nn.ReLU(True),                                #ReLU activation function - Activation function determinines if a neuron fires, i.e is the output of the node considered usefull. also allows for backprop. the arg 'True' makes the opperation carry out in-place(changes the values in the input array to the output values rather than making a new one), default would be false
             #Linear encoder layer 2
-            nn.Linear(128, encoded_space_dim)             #Takes in data of 1 dimension with size 128 and outputs it in one dimension of size defined by encoded_space_dim (this is the latent space? the smalle rit is the more comression but thte worse the final fidelity)
+            nn.Linear(self.fc_layer_size, encoded_space_dim)             #Takes in data of 1 dimension with size 128 and outputs it in one dimension of size defined by encoded_space_dim (this is the latent space? the smalle rit is the more comression but thte worse the final fidelity)
         )
 
     def forward(self, x):
@@ -74,26 +76,28 @@ class Encoder(nn.Module):
         x = self.encoder_lin(x)                           #Runs linear encoder on flattened output 
         if self.encoder_debug == 1:
             print("Encoder Lin out", x.size(),"\n")
-            self.encoder_debug = 0    #????? what is this line for     
+            self.encoder_debug = 0                    #Turns off debug printouts after first run   
         
         return x                                          #Return final result
 
 #%% - Decoder
 class Decoder(nn.Module):
     
-    def __init__(self, encoded_space_dim, decoder_debug):
+    def __init__(self, encoded_space_dim, decoder_debug, fc_layer_size=128):
         super().__init__()
 
         # Enables decoder layer shape debug printouts
         self.decoder_debug = decoder_debug
 
+        self.fc_layer_size = fc_layer_size
+
         ###Linear Decoder Layers
         self.decoder_lin = nn.Sequential(
             #Linear decoder layer 1            
-            nn.Linear(encoded_space_dim, 128),
+            nn.Linear(encoded_space_dim, self.fc_layer_size),
             nn.ReLU(True),                                 #ReLU activation function - Activation function determinines if a neuron fires, i.e is the output of the node considered usefull. also allows for backprop. the arg 'True' makes the opperation carry out in-place(changes the values in the input array to the output values rather than making a new one), default would be false
             #Linear decoder layer 2
-            nn.Linear(128, 32 * 16 * 11),
+            nn.Linear(self.fc_layer_size, 32 * 16 * 11),
             nn.ReLU(True)                                  #ReLU activation function - Activation function determinines if a neuron fires, i.e is the output of the node considered usefull. also allows for backprop. the arg 'True' makes the opperation carry out in-place(changes the values in the input array to the output values rather than making a new one), default would be false
         )
         ###Unflatten layer
@@ -131,7 +135,7 @@ class Decoder(nn.Module):
             print("Decoder CNN out", x.size(),"\n")            
             self.decoder_debug = 0         
         
-        x = torch.sigmoid(x)          #THIS IS IMPORTANT PART OF FINAL OUTPUT!: Runs sigmoid function which turns the output data values to range (0-1)#!!! ????    Also can use tanh fucntion if wanting outputs from -1 to 1
+        x = torch.sigmoid(x)          # Runs sigmoid function which turns the output data values to range (0-1)  Also can use tanh fucntion if wanting outputs from -1 to 1
 
         # cropping 10 pixels from each edge of the output
         x = x[:, :, 4:-4, 4:-4]
